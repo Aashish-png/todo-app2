9ed6fcb867354f3b1ebaf3154f79c23e8c263ece
aashish <aashishsarwa512@gmail.com>
2022-03-10 18:58:29 +0530

    pushed 2

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+
+import { AppComponent } from './app.component';
+import { TodosComponent } from './components/todos/todos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodosComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    CommonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
